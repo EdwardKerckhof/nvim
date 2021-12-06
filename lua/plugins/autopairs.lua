91225a8a26e916a05efb25cf8c1c6350f1038156
Edward Kerckhof <edward@zonedev.be>
2021-12-06 20:07:18 +0100

    Fix autopairs

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -8,7 +8,13 @@ M.config = function ()
   end
 
   autopairs.setup({
-
+    map_cr = true,
+    check_ts = true,
+    ts_config = {
+      lua = {'string'},-- it will not add a pair on that treesitter node
+      javascript = {'template_string'},
+      java = false,-- don't check treesitter on java
+    }
   })
 end
 
