a81e2cf74fabb309b65b38e49e4c19d7645cd416
EdwardKerckhof <edward@zonedev.be>
2021-12-13 02:12:49 +0100

    Fix packer autocompile

--- lua/plugins/terminal.lua
+++ lua/plugins/terminal.lua
@@ -22,7 +22,7 @@ M.setup = function()
 end
 
 local Terminal = require("toggleterm.terminal").Terminal
-local lazygit = Terminal:new({cmd = "lazygit", hidden = true, direction = "float" })
+local lazygit = Terminal:new({cmd = "lazygit", hidden = true, direction = "float"})
 
 function _LAZYGIT_TOGGLE()
   lazygit:toggle()
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -4,14 +4,15 @@ local fn = vim.fn
 local install_path = fn.stdpath('data') .. '/site/pack/packer/start/packer.nvim'
 if fn.empty(fn.glob(install_path)) > 0 then
   PACKER_BOOTSTRAP = fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
-  print('Installing packer close and reopen Neovim...')
+  print("Installing packer close and reopen Neovim...")
+  vim.cmd [[packadd packer.nvim]]
 end
 
 -- Autocommand that reloads neovim whenever you save the plugins.file
 vim.cmd([[
   augroup packer_user_config
     autocmd!
-    autocmd BufWritePost plugins.source <afile> | PackerSync
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
   augroup end
 ]])
 
