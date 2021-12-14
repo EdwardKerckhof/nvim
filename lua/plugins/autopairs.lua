948ed7dba618e0172f60289505b732e22fa42bed
EdwardKerckhof <edward@zonedev.be>
2021-12-14 12:44:06 +0100

    Update autopairs and treesitter

--- lua/plugins/autopairs.lua
+++ lua/plugins/autopairs.lua
@@ -1,19 +1,42 @@
 -- Autopairs (https://github.com/windwp/nvim-autopairs)
 local status_ok, autopairs = pcall(require, "nvim-autopairs")
-if not status_ok then return end
+if not status_ok then
+  return
+end
 
 local M = {}
 
 M.setup = function()
-  local cmpap_ok, cmp_autopairs = pcall(require, 'nvim-autopairs.completion.cmp')
-  if not cmpap_ok then return end
-  local cmp_ok, cmp = pcall(require, 'cmp')
-  if not cmp_ok then return end
+  local cmpap_ok, cmp_autopairs = pcall(require, "nvim-autopairs.completion.cmp")
+  if not cmpap_ok then
+    return
+  end
+  local cmp_ok, cmp = pcall(require, "cmp")
+  if not cmp_ok then
+    return
+  end
 
-  cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done({map_char = {tex = ''}}))
-  cmp_autopairs.lisp[#cmp_autopairs.lisp + 1] = "racket"
+  cmp.event:on("confirm_done", cmp_autopairs.on_confirm_done { map_char = { tex = "" } })
 
-  autopairs.setup({check_ts = true})
+  autopairs.setup {
+    check_ts = true,
+    ts_config = {
+      lua = { "string", "source" },
+      javascript = { "string", "template_string" },
+    },
+    disable_filetype = { "TelescopePrompt", "spectre_panel" },
+    fast_wrap = {
+      map = "<M-e>",
+      chars = { "{", "[", "(", '"', "'" },
+      pattern = string.gsub([[ [%'%"%)%>%]%)%}%,] ]], "%s+", ""),
+      offset = 0, -- Offset from pattern match
+      end_key = "$",
+      keys = "qwertyuiopzxcvbnmasdfghjkl",
+      check_comma = true,
+      highlight = "PmenuSel",
+      highlight_grey = "LineNr",
+    },
+  }
 end
 
 return M
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,35 +1,42 @@
 -- Treesitter (https://github.com/nvim-treesitter/nvim-treesitter)
 local status_ok, treesitter = pcall(require, "nvim-treesitter.configs")
-if not status_ok then return end
+if not status_ok then
+  return
+end
 
 local M = {}
 
 M.setup = function()
-
   -- Fix for issue: https://github.com/windwp/nvim-ts-autotag/issues/19
-  vim.lsp.handlers['textDocument/publishDiagnostics'] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+  vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
     underline = true,
-    virtual_text = {spacing = 5, severity_limit = 'Warning'},
-    update_in_insert = true
+    virtual_text = { spacing = 5, severity_limit = "Warning" },
+    update_in_insert = true,
   })
 
   treesitter.setup {
     ensure_installed = "maintained",
     sync_install = false, -- install languages synchronously (only applied to `ensure_installed`)
-    highlight = {enable = true, additional_vim_regex_highlighting = true},
-    indent = {enable = true, disable = {"yaml"}},
+    highlight = { -- treesitter highlighting
+      enable = true,
+      additional_vim_regex_highlighting = true,
+    },
+    indent = { -- indent blankline
+      enable = true,
+      disable = { "yaml" },
+    },
     autotag = { -- nvim-ts-autotag plugin
-      enable = true
+      enable = true,
     },
     rainbow = { -- nvim-ts-rainbow plugin
       enable = true,
-      extended_mode = false
+      extended_mode = false,
     },
     context_commentstring = { -- comment based on cursor location
       enable = true,
-      enable_autocmd = false
+      enable_autocmd = false,
     },
-    matchup = {enable = true}
+    matchup = { enable = true },
   }
 end
 
