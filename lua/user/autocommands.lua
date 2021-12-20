40e44d372e2947cfe025cdbd3b68bd83d5bc23f8
EdwardKerckhof <edward@zonedev.be>
2021-12-20 12:40:12 +0100

    Small updates

--- lua/user/autocommands.lua
+++ lua/user/autocommands.lua
@@ -3,9 +3,6 @@ vim.cmd [[
   augroup _general_settings
     autocmd!
     autocmd FileType qf,help,man,lspinfo,spectre_panel nnoremap <silent> <buffer> q :close<CR>
-    autocmd TextYankPost * silent!lua require('vim.highlight').on_yank({higroup = 'Visual', timeout = 200}) 
-    autocmd BufWinEnter * :set formatoptions-=cro
-    autocmd FileType qf set nobuflisted
   augroup end
 
   augroup _git
@@ -56,6 +53,7 @@ vim.cmd [[
   augroup _autoFormat
     autocmd!
     autocmd BufWritePre * lua vim.lsp.buf.formatting() 
+    autocmd BufWritePost * :w
     autocmd BufWritePre *.ts,*.css,*.html,*.tsx,*.js,*.vue :EslintFixAll
   augroup END
 ]]
--- lua/user/options.lua
+++ lua/user/options.lua
@@ -2,7 +2,7 @@ local options = {
   backup = false, -- creates a backup file
   clipboard = "unnamedplus", -- allows neovim to access the system clipboard
   cmdheight = 2, -- more space in the neovim command line for displaying messages
-  completeopt = {"menuone", "noselect"}, -- mostly just for cmp
+  completeopt = { "menuone", "noselect" }, -- mostly just for cmp
   conceallevel = 0, -- so that `` is visible in markdown files
   fileencoding = "utf-8", -- the encoding written to a file
   hidden = true, -- required to keep multiple buffers open
@@ -24,7 +24,7 @@ local options = {
   expandtab = true, -- convert tabs to spaces
   shiftwidth = 2, -- the number of spaces inserted for each indentation
   tabstop = 2, -- insert 2 spaces for a tab
-  cursorline = true, -- highlight the current line
+  cursorline = false, -- highlight the current line
   number = true, -- set numbered lines
   relativenumber = true, -- set relative numbered lines
   numberwidth = 2, -- set number column width to 2 {default 4}
@@ -32,17 +32,20 @@ local options = {
   wrap = true, -- wraps lines
   scrolloff = 8, -- scroll offset (8 lines)
   sidescrolloff = 8,
-  guifont = "mononoki:h13", -- the font used in graphical neovim applications
+  guifont = "JetBrains Mono Nerd Font:h13", -- the font used in graphical neovim applications
   spell = true,
-  spelllang = "en"
+  spelllang = "en",
 }
 
 vim.opt.shortmess:append "c"
 
 -- Notifications
-vim.notify = require('notify')
+vim.notify = require "notify"
 
-for k, v in pairs(options) do vim.opt[k] = v end
+for k, v in pairs(options) do
+  vim.opt[k] = v
+end
 
-vim.cmd('filetype plugin indent on')
+vim.cmd "filetype plugin indent on"
 vim.cmd "set whichwrap+=<,>,[,],h,l"
+vim.cmd [[set iskeyword+=-]]
